58df93eeed22ff607dd15af35d597f561b2158e0
Harsh Mistry <harshm@zignuts.com-at-075929790289>
2024-03-01 11:56:16 +0530

    version changed 1.9 and lib added

--- app/src/main/java/com/example/demosdkuploadandroid/MainActivity.kt
+++ app/src/main/java/com/example/demosdkuploadandroid/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.demosdkuploadandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import com.example.mylibrarydemo.DemoClass
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val demo = DemoClass("hkm")
+        val name = demo.getName()
+        lifecycleScope.launch{
+            val runtimeVersion = demo.getRuntimeVersion()
+            Log.e("Name and Runtime Version","Name and Runtime Version ${name}/${runtimeVersion}")
+        }
     }
 }
--- mylibrarydemo/src/main/java/com/example/mylibrarydemo/DemoClass.kt
+++ mylibrarydemo/src/main/java/com/example/mylibrarydemo/DemoClass.kt
@@ -1,7 +1,46 @@
 package com.example.mylibrarydemo
 
+import com.google.gson.Gson
+import com.neovisionaries.ws.client.WebSocketFactory
+import jp.co.soramitsu.fearless_utils.wsrpc.SocketService
+import jp.co.soramitsu.fearless_utils.wsrpc.executeAsync
+import jp.co.soramitsu.fearless_utils.wsrpc.interceptor.WebSocketResponseInterceptor
+import jp.co.soramitsu.fearless_utils.wsrpc.recovery.Reconnector
+import jp.co.soramitsu.fearless_utils.wsrpc.request.RequestExecutor
+import jp.co.soramitsu.fearless_utils.wsrpc.request.runtime.RuntimeRequest
+import jp.co.soramitsu.fearless_utils.wsrpc.response.RpcResponse
+
 class DemoClass(private val name : String) {
     fun getName() : String {
         return name
     }
+   private var socketService: SocketService? = null
+    init {
+        val reconnector = Reconnector()
+        val requestExecutor = RequestExecutor()
+        socketService = SocketService(
+            Gson(),
+            LoggerImpl(),
+            WebSocketFactory(),
+            reconnector,
+            requestExecutor
+        )
+        socketService?.setInterceptor(object : WebSocketResponseInterceptor {
+            override fun onRpcResponseReceived(rpcResponse: RpcResponse): WebSocketResponseInterceptor.ResponseDelivery {
+                return WebSocketResponseInterceptor.ResponseDelivery.DELIVER_TO_SENDER
+            }
+        })
+        socketService?.start("wss://wsspc1-qa.agung.peaq.network/")
+    }
+    suspend fun getRuntimeVersion() : String {
+
+        val resultRuntimeVersion = socketService?.executeAsync(
+            RuntimeRequest(
+                method = "chain_getRuntimeVersion",
+                params = listOf()
+            )
+        )
+        return resultRuntimeVersion?.result.toString()
+    }
+
 }
